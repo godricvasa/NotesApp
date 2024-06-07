258fa223dbbddf9aaf963f280e8a37d48ebd5837
Vasanth212004 <112002007+Vasanth212004@users.noreply.github.com>
2024-06-07 19:40:19 +0530

    added state store for marking task

--- src/store.js
+++ src/store.js
@@ -12,7 +12,12 @@ function rootReducer(state = initialState, action) {
       return { ...state, notes: state.notes.filter((_, index) => index !== action.payload) };
     case 'EDIT_NOTE':
       return { ...state, notes: state.notes.map((item, index) => index === action.id ? action.payload : item) };
-    default:
+      case 'TOGGLE_COMPLETE':
+        return {
+          ...state,
+          notes: state.notes.map((note, index) => index === action.payload ? { ...note, completed: !note.completed } : note)
+        };
+      default:
       return state;
   }
 }
