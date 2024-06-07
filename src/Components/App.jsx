f63b45bc811a5d80ce70237507b688df620ffb23
Vasanth212004 <112002007+Vasanth212004@users.noreply.github.com>
2024-06-07 17:53:24 +0530

    redux store was added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,39 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Notes";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const notes = useSelector(state => state.notes);
+  const dispatch = useDispatch();
 
   function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
+    dispatch({ type: 'ADD_NOTE', payload: newNote });
   }
 
   function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
+    dispatch({ type: 'DELETE_NOTE', payload: id });
   }
+
   function editNote(id, updatedNote) {
-    setNotes(prevNotes => {
-      return prevNotes.map((noteItem, index) => {
-        return index === id ? updatedNote : noteItem;
-      });
-    });
+    dispatch({ type: 'EDIT_NOTE', id, payload: updatedNote });
   }
-  
 
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {/* <button onClick={addNote}>add</button> */}
       {notes.map((noteItem, index) => {
         return (
           <Note
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import Header from './Components/Header';
+import { Provider } from 'react-redux';
 import App from './Components/App';
-
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <App />
+      <Provider store={store}><App /></Provider>
+    
     </div>
 );
 
